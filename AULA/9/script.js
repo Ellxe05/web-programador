66f4a2b14c52b23fb4d4017e886d5cde0e0b8e3e
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-02-14 12:59:41 -0300

    object

---
+++ AULA/9/script.js
@@ -0,0 +1,14 @@
+var Gabrielle = {
+    nome: 'Gabrielle',
+    sobreNome: 'Silva',
+    notas: [7.5, 10]
+}
+
+var Graziele = {
+    nome: 'Graziele',
+    sobreNome: 'Rodrigues',
+    notas: [9.5, 10] 
+}
+
+console.log(Gabrielle.nome)
+console.log(Graziele.sobreNome)
