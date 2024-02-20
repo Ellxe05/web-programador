e790a6eae1b2662a4ac7e50d5d26a0e62ff5cf8d
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-02-20 17:33:30 -0300

    canva - retangulo

---
+++ AULA/22/script3.js
@@ -0,0 +1,11 @@
+let tela = document.getElementById('tela')
+let ctx = tela.getContext('2d')
+
+ctx.rect(10, 10, 100, 200)
+ctx.fillStyle = 'darkgreen'
+ctx.lineWidth = 5
+ctx.strokeStyle = 'yellow'
+ctx.fill()
+ctx.stroke()
+
+ctx.clearRect(20, 20, 30, 30)
