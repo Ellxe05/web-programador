4352d2fbd110a5587c95281b88b4e887453b6d10
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-02-20 12:37:58 -0300

    atributos personalizados

--- AULA/20/script2.js
+++ AULA/20/script2.js
@@ -1,2 +1,9 @@
 let lista = document.getElementById('lista')
-let num = lista.getAttribute('num')
+let num = parseInt(lista.getAttribute('num'))
+let conteudo = ''
+
+for (let i = 1; i < num; i++) {
+    conteudo += '<li>' + i + '</li>'
+}
+
+lista.innerHTML = conteudo
