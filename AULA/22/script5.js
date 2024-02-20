6dcc58011576132af2a6bcd8605210fd90e399f6
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-02-20 18:40:17 -0300

    ANIMAÇÃO circulo - canvas

--- AULA/22/script5.js
+++ AULA/22/script5.js
@@ -1,15 +1,34 @@
 let tela = document.getElementById('tela')
 let ctx = tela.getContext('2d')
-let x = 250
-let y = 250
-let raio = 100
-let inicio = 0
-let fim = 2 * Math.PI
+let circle = {
+    x: 250,
+    y: 250,
+    raio: 100,
+    inicio: 0,
+    fim: 0
+}
 
-ctx.beginPath()
-ctx.strokeStyle = 'lightgreen'
-ctx.fillStyle = 'yellow'
-ctx.arc(x, y, raio, inicio, fim)
+function drawCircle(c){
+    // PRO CIRCULO PARAR DE ANDAR SO EXCLUIR ISSO
+    ctx.beginPath()
+    ctx.rect(0, 0, 500, 500)
+    ctx.fillStyle = 'rgb(83, 83, 71)'
+    ctx.fill()
+    // E VOLTAR O BACKGROUNDCOLOR DO CSS
 
-ctx.fill()
-ctx.stroke()
+    ctx.beginPath()
+    ctx.strokeStyle = 'lightgreen'
+    ctx.fillStyle = 'yellow'
+    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim)
+
+    ctx.fill()
+    ctx.stroke()
+}
+
+setInterval(function() {
+    if (circle.fim < 2 * Math.PI) {
+        circle.fim += 0.3
+        circle.x += 4
+    }
+    drawCircle(circle)
+}, 40)
