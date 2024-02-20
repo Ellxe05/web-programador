4c2e0fe54d6cfc819df30fac19cb495f20314817
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-02-20 12:59:05 -0300

    atrib. pers. 2

--- AULA/20/script2.js
+++ AULA/20/script2.js
@@ -1,5 +1,7 @@
 let lista = document.getElementById('lista')
-let num = parseInt(lista.getAttribute('num'))
+lista.dataset.num = 20
+
+let num = parseInt(lista.dataset.num)
 let conteudo = ''
 
 for (let i = 1; i < num; i++) {
